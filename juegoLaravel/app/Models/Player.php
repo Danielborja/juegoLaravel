05e035dc5e4ca2432c5316b2b74639558aad7537
daniel <danielborjaperez99@gmail.com>
2023-04-21 12:35:48 +0200

    sisisi

--- juegoLaravel/app/Models/Player.php
+++ juegoLaravel/app/Models/Player.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Stat;
 class Player extends Model
 {
     use HasFactory;
+    protected $fillable = ['name','password'];
+    public function user(){
+        return $this->hasOne(Stat::class);
+    }
 }
--- juegoLaravel/app/Models/Stat.php
+++ juegoLaravel/app/Models/Stat.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Player;
 
 class Stat extends Model
 {
     use HasFactory;
+    protected $fillable = ['wins','losts', 'player_id'];
+    public function category(){
+        return $this->belongsTo(Player::class);
+    }
 }
