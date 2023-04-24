f25fe6596095048b318378cabe261ca0b282ad21
daniel <danielborjaperez99@gmail.com>
2023-04-24 13:58:46 +0200

    13:58

---
+++ app/Http/Controllers/JuegoController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class JuegoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+
+     public function random()
+    {
+
+        return rand(0,2);
+
+    }
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(Request $request)
+    {
+        
+        if ($request->has('piedra')) {
+            $valorBoton = 0;
+        } elseif ($request->has('papel')) {
+            $valorBoton = 1;
+        } elseif ($request->has('tijera')) {
+            $valorBoton = 2;
+        }
+        
+        $maquina = $this -> random();
+
+        if ($valorBoton = 0 and $maquina = 0) {    
+
+        }
+        
+        elseif ($valorBoton = 0 and $maquina = 1) {
+            return view ("gameover",compact('maquina'));
+        }
+        elseif ($valorBoton = 0 and $maquina = 2) {
+            return view ("winner",compact('maquina'));
+        }
+        elseif ($valorBoton = 1 and $maquina = 0) {
+            return view ("winner",compact('maquina'));
+        }
+        elseif ($valorBoton = 1 and $maquina = 1) {
+            
+        }
+        elseif ($valorBoton = 1 and $maquina = 2) {
+            return view ("gameover",compact('maquina'));
+        }
+        elseif ($valorBoton = 2 and $maquina = 0) {
+            return view ("gameover",compact('maquina'));
+        }
+        elseif ($valorBoton = 2 and $maquina = 1) {
+            return view ("winner",compact('maquina'));
+        }
+        elseif ($valorBoton = 2 and $maquina = 2) {
+            
+        }
+
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- resources/views/juego.blade.php
+++ resources/views/juego.blade.php
@@ -7,6 +7,10 @@
     <title>Document</title>
 </head>
 <body>
-    <p>Se ha logueado correctamente</p>
+    <center>
+    <button style="width: 150px; height: 50px;" value="0"; name="piedra">✊</button>
+    <button style="width: 150px; height: 50px;" value="1"; name="papel">✋</button>
+    <button style="width: 150px; height: 50px;" value="2" ;name="tijera">✌</button>
+    </center>
 </body>
 </html>
