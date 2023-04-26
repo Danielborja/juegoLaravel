43c707b0687e2f2b899d30695f0d300095941a8e
Danielborja <126692666+Danielborja@users.noreply.github.com>
2023-04-26 09:25:43 +0200

    Create gameover.blade.php

---
+++ resources/views/gameover.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>perdiste</h1>
+<h1>la maquina eligió:{{$maquina}}</h1>
+    <ul>
+        <li><a href="{{ route('try_again') }}">try again</a></li>
+        </ul>
+</body>
+</html>
